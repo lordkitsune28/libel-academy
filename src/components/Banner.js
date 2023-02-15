ced0721ddb44e2e75424250669a869d2b90c4593
carlos medina <carlosassasin1505@gmail.com>
2023-02-15 10:11:18 -0500

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 export const Banner = () => {
-  return (
-    <div>Banner</div>
-  )
+    return (
+        <div>
+            <div>
+                <h1>Big Comeback</h1>
+                <p>Nulla porta, eros idaliquam pulvinar, urna ex mattis eros, ves-
+                    tibulum urna turpis et risus. Mauris porttitor risus faucibus, auctor
+                    arcu a, tincidunt nibh...</p>
+            </div>
+        </div>
+    )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './container/App';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
+import "./styles/style.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
