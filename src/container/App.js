1743ecb25bc447d95edf83930b21b35049779539
carlos medina <carlosassasin1505@gmail.com>
2023-02-15 09:44:21 -0500

    banner

---
+++ src/container/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Banner } from '../components/Banner'
+
+
+export const App = () => {
+  return (
+    <div>
+        <Banner/>
+    </div>
+  )
+}
